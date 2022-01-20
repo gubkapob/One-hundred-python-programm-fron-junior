d6acbfde8116486fd9e0f9bac57bc5d4aea212d9
Nik <82082074+gubkapob@users.noreply.github.com>
2022-01-20 14:21:04 +0300

    Function

---
+++ Hidden password.py
@@ -0,0 +1,12 @@
+"""
+This programm in Python get password value and hidding first
+and last characters / Python version >= 3.8
+"""
+
+def hidding_password(password):
+    
+    stars = "*" * (len(password)-2)
+    result = password[0] + stars + password[-1]
+    return result
+
+print(hidding_password(input("Enter password:--")))
